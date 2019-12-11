a25dd5688ad6731447f1933146d9b110166678c3
liutengfei <liutf@riverroad.cn>
2019-12-11 14:10:15 +0800

    关联远程仓库

---
+++ src/test/java/com/bbq/skill/SkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbq.skill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
